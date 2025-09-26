2d223f79029ed79d7e9e4f0dc685f100f1c41c08
Hiranya Mendis <hiranyamendis52@gmail.com>
2025-09-26 11:10:00 +0530

    Add project dependencies and configure JavaFX SDK

--- src/main/java/Starter.java
+++ src/main/java/Starter.java
@@ -1,2 +1,13 @@
-public class Starter {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Starter extends Application {
+    public static void main(String[] args) {
+        
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
 }
