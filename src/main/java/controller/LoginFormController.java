6138c77070b8fe85875da23a3d1ab8f511ccf4a9
Hiranya Mendis <hiranyamendis52@gmail.com>
2025-09-28 00:37:17 +0530

    Update login page design

--- src/main/java/controller/LoginFormController.java
+++ src/main/java/controller/LoginFormController.java
@@ -1,4 +1,27 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class LoginFormController {
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    void btnLogInOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtRegisterOnAction(MouseEvent event) {
+
+    }
+
 }
