5e41606a5e010f66c2f86a6cc199f639b1a8eae0
Hiranya Mendis <hiranyamendis52@gmail.com>
2025-09-28 01:37:35 +0530

    Design register form page

--- src/main/java/controller/LoginFormController.java
+++ src/main/java/controller/LoginFormController.java
@@ -2,9 +2,16 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginFormController {
 
@@ -15,13 +22,13 @@ public class LoginFormController {
     private TextField txtUsername;
 
     @FXML
-    void btnLogInOnAction(ActionEvent event) {
+    void btnLoginOnAction(ActionEvent event) {
 
     }
 
     @FXML
     void txtRegisterOnAction(MouseEvent event) {
-
+        
     }
 
 }
--- src/main/java/controller/RegisterFormController.java
+++ src/main/java/controller/RegisterFormController.java
@@ -1,4 +1,37 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class RegisterFormController {
+
+    @FXML
+    private PasswordField txtConfirmPassword;
+
+    @FXML
+    private PasswordField txtCreatePassword;
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    void btnRegisterOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void lblLogInOnEnter(MouseEvent event) {
+
+    }
+
 }
